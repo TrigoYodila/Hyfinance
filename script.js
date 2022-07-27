6cd958f642818a71f8357289655b399e335aa08f
Trigo YODILA <trigoyodila1996@gmail.com>
2022-07-27 19:58:00 +0200

    add latest news

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 var swiper = new Swiper(".home-slider", {
   loop: true,
   grabCursor: true,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
 
+
+var swiper = new Swiper(".news-card", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
